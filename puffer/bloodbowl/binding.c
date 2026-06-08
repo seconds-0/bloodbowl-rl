708ee0cc6a5485cc23979426a1a48ebd2d2fd2fd
huth-stacks <230392931+huth-stacks@users.noreply.github.com>
2026-06-07 23:32:03 -0700

    binding.c OBS_SIZE 1612 -> 2782 (the static-assert caught the v4 size skew)

--- puffer/bloodbowl/binding.c
+++ puffer/bloodbowl/binding.c
@@ -5,7 +5,7 @@
 // defines as plain literals.
 #include "bloodbowl.h"
 
-#define OBS_SIZE 1612
+#define OBS_SIZE 2782 // obs v4 (mirrors BBE_OBS_SIZE; static-assert below)
 #define NUM_ATNS 3
 #define ACT_SIZES {30, 33, 391}
 #define OBS_TENSOR_T ByteTensor
